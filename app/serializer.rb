7ae720c1af6c3a1aa99264b1b97e4a0be50fafa9
Pradeep Soni <Pradeep.soni@protonshub.in>
2022-10-06 19:21:07 +0530

    Submission for interview test

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,33 @@
 class Serializer
+ 
+ def self.fields
+    @fields
+ end
+
+  def self.object
+    @object
+  end 	
+
+ def self.attribute(attr_name,&block)    
+    @fields =  @fields || []        
+    @fields_with_block = []
+   	 if block.nil?     	       
+       	@fields << attr_name       
+   	else
+	   	@fields_with_block << [attr_name,block]
+    end
+ end
+
+  def self.new(object)
+  	 @object = object
+  	 self 
+  end
+
+ def self.serialize 
+   json = {} 		
+   @fields.each{|key| json.merge!("#{key.to_s}": @object["#{key}"]) }		
+   @fields_with_block.each{|key| json.merge!("#{key[0].to_s}": key[1].call(@object))}
+   json
+ end
+
 end
